3f883d5cf151a9a31aad473b3e1d381e2fce8a02
Tri Nguyen <tringuyenm@greystone.com>
2020-03-18 17:27:38 +0700

    Add Collapsing toolbar

---
+++ app/src/main/java/gdsvn/tringuyen/moviesreview/presentation/common/Define.kt
@@ -0,0 +1,8 @@
+package gdsvn.tringuyen.moviesreview.presentation.common
+
+class Define {
+    companion object {
+        const val URL_LOAD_IMAGE =  "https://image.tmdb.org/t/p/w500"
+    }
+
+}
--- app/src/main/java/gdsvn/tringuyen/moviesreview/presentation/ui/fragment/movies/MoviesFragment.kt
+++ app/src/main/java/gdsvn/tringuyen/moviesreview/presentation/ui/fragment/movies/MoviesFragment.kt
@@ -1,25 +1,34 @@
 package gdsvn.tringuyen.moviesreview.presentation.ui.fragment.movies
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import gdsvn.tringuyen.moviesreview.R
+import gdsvn.tringuyen.moviesreview.data.local.model.Movie
 import gdsvn.tringuyen.moviesreview.data.remote.paging.common.State
 import gdsvn.tringuyen.moviesreview.data.remote.paging.now.MoviesNowListAdapter
 import gdsvn.tringuyen.moviesreview.data.remote.paging.top.MoviesTopListAdapter
+import gdsvn.tringuyen.moviesreview.presentation.common.Define
 import gdsvn.tringuyen.moviesreview.presentation.vm.popular.MoviesPopularViewModel
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.movies_fragment.*
 import kotlinx.android.synthetic.main.movies_fragment_main_layout.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
 
 class MoviesFragment : Fragment() {
 
@@ -27,7 +36,6 @@ class MoviesFragment : Fragment() {
     private lateinit var mMoviesListAdapter: MoviesTopListAdapter
     private lateinit var mMoviesNowListAdapter: MoviesNowListAdapter
     private lateinit var mMoviesTopListAdapter: MoviesTopListAdapter
-    private val imageLink = "https://image.tmdb.org/t/p/w500"
 
     companion object {
         fun newInstance() = MoviesFragment()
@@ -47,13 +55,15 @@ class MoviesFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        setupView(false)
+        //set app is fullscreen
+        this.activity?.window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        showListMovie(false)
         setupToolbar()
         initAdapter()
         initState()
     }
 
-    private fun setupView(isShow: Boolean) {
+    private fun showListMovie(isShow: Boolean) {
         if(isShow) {
             textview_now.visibility =View.VISIBLE
             textview_popular.visibility =View.VISIBLE
@@ -63,34 +73,10 @@ class MoviesFragment : Fragment() {
             textview_popular.visibility =View.GONE
             textview_top.visibility =View.GONE
         }
-
     }
 
     private fun setupToolbar() {
         handleCollapsedToolbarTitle()
-    }
-
-    /**
-     * sets the title on the toolbar only if the toolbar is collapsed
-     */
-    private fun handleCollapsedToolbarTitle() {
-        this.appbar.addOnOffsetChangedListener(object : OnOffsetChangedListener {
-            var isShow = true
-            var scrollRange = -1
-            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-                if (scrollRange == -1) {
-                    scrollRange = appBarLayout.totalScrollRange
-                }
-                // verify if the toolbar is completely collapsed and set the movie name as the title
-                if (scrollRange + verticalOffset == 0) {
-                    this@MoviesFragment.collapsing_toolbar.title = this@MoviesFragment.getString(R.string.movies)
-                    isShow = true
-                } else if (isShow) { // display an empty string when toolbar is expanded
-                    this@MoviesFragment.collapsing_toolbar.title = ""
-                    isShow = false
-                }
-            }
-        })
     }
 
     private fun initAdapter() {
@@ -105,12 +91,14 @@ class MoviesFragment : Fragment() {
         mMoviesNowListAdapter = MoviesNowListAdapter { moviesPopularViewModel.retry() }
         second_recycler_view.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
         second_recycler_view.adapter = mMoviesNowListAdapter
-        moviesPopularViewModel.movieList.observe(this, Observer {
-            Glide.with(this)
-                .load(imageLink + it[0]!!.backdrop_path)
-                .into(image_movie_backdrop)
-            mMoviesNowListAdapter.submitList(it)
-            setupView(true)
+        moviesPopularViewModel.movieList.observe(this, Observer { listMovie ->
+
+            mMoviesNowListAdapter.submitList(listMovie)
+            if(!listMovie.isEmpty()) {
+                showPoster(listMovie)
+                showListMovie(true)
+            }
+
         })
 
         mMoviesTopListAdapter = MoviesTopListAdapter { moviesPopularViewModel.retry() }
@@ -122,19 +110,71 @@ class MoviesFragment : Fragment() {
 
     }
 
-
     private fun initState() {
         txt_error.setOnClickListener { moviesPopularViewModel.retry() }
         moviesPopularViewModel.getState().observe(this, Observer { state ->
             progress_bar.visibility = if (moviesPopularViewModel.listIsEmpty() && state == State.LOADING) View.VISIBLE else View.GONE
             txt_error.visibility = if (moviesPopularViewModel.listIsEmpty() && state == State.ERROR) View.VISIBLE else View.GONE
+            if (moviesPopularViewModel.listIsEmpty() && state == State.ERROR) {
+                showListMovie(false)
+            }
             if (!moviesPopularViewModel.listIsEmpty()) {
                 mMoviesListAdapter.setState(state ?: State.DONE)
-
             }
         })
     }
 
+    /**
+     * Sets 20 poster on the imageView every 5(s) with animation. Maybe I will change imageView to Recycle view to load all image.
+     */
+    @SuppressLint("CheckResult")
+    private fun showPoster(listMovie: PagedList<Movie>) {
+        Timber.d("Start show poster ---------------------------------")
+        Observable.interval(0,5, TimeUnit.SECONDS)
+            .map { i -> listMovie[i.toInt()] }
+            .take(listMovie.size.toLong())
+            .repeatWhen { completed -> completed.delay(2, TimeUnit.SECONDS) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {movie ->
+                val posterURL = Define.URL_LOAD_IMAGE + movie!!.backdrop_path
+                val animFadeIn: Animation = AnimationUtils.loadAnimation(
+                    this.context,
+                    R.anim.fade_in
+                )
+                image_movie_backdrop.startAnimation(animFadeIn)
 
+                Glide.with(this)
+                    .load(posterURL)
+                    .into(image_movie_backdrop);
+
+            }
+    }
+
+    /**
+     * Sets the title on the toolbar only if the toolbar is collapsed
+     */
+    private fun handleCollapsedToolbarTitle() {
+        this.appbar.addOnOffsetChangedListener(object : OnOffsetChangedListener {
+            var isShow = true
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                // verify if the toolbar is completely collapsed and set the movie name as the title
+                if (scrollRange + verticalOffset == 0) {
+                    this@MoviesFragment.collapsing_toolbar.title = this@MoviesFragment.getString(R.string.movies)
+                    this@MoviesFragment.collapsing_toolbar.setCollapsedTitleTextColor(resources.getColor(R.color.text_light))
+                    this@MoviesFragment.collapsing_toolbar.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+                    isShow = true
+                } else if (isShow) { // display an empty string when toolbar is expanded
+                    this@MoviesFragment.collapsing_toolbar.title = this@MoviesFragment.getString(R.string.movies)
+                    this@MoviesFragment.collapsing_toolbar.setExpandedTitleColor(resources.getColor(R.color.text_light))
+                    this@MoviesFragment.collapsing_toolbar.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+                    isShow = false
+                }
+            }
+        })
+    }
 
 }
